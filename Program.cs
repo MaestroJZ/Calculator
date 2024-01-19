0e9beb87febd9f570e67e5e527f7068b7093fcdd
MaestroJZ <eldar_02_03@outlook.com>
2024-01-19 14:28:48 +0600

    Проверка для char

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     private static void Main(string[] args)
     {
         double a, b , result;
-
+        char o;
         Console.Write("a = ");
         if (double.TryParse(Console.ReadLine(), out a))
         {
@@ -11,42 +11,38 @@
             if (double.TryParse(Console.ReadLine(), out b))
             {
                 Console.Write("Операция: ");
-                char o = Convert.ToChar(Console.ReadLine());
-                switch (o)
+                if (char.TryParse(Console.ReadLine(), out o))
                 {
-                    case '+':
-                        result = a + b;
-                        Console.WriteLine($"{a} + {b} = {result}");
-                        break;
-                    case '-':
-                        result = a - b;
-                        Console.WriteLine($"{a} - {b} = {result}");
-                        break;
-                    case '*':
-                        result = a * b;
-                        Console.WriteLine($"{a} * {b} = {result}");
-                        break;
-                    case '/':
-                        if (b == 0) { Console.WriteLine("На 0 нельзя делить"); }
-                        else
-                        {
-                            result = a / b;
-                            Console.WriteLine($"Результат: {result}");
-                        }
-                        break;
-                    default:
-                        Console.WriteLine($"Неправильно ввели операцию");
-                        break;
-                }
+                    switch (o)
+                    {
+                        case '+':
+                            result = a + b;
+                            Console.WriteLine($"{a} + {b} = {result}");
+                            break;
+                        case '-':
+                            result = a - b;
+                            Console.WriteLine($"{a} - {b} = {result}");
+                            break;
+                        case '*':
+                            result = a * b;
+                            Console.WriteLine($"{a} * {b} = {result}");
+                            break;
+                        case '/':
+                            if (b == 0) { Console.WriteLine("На 0 нельзя делить"); }
+                            else
+                            {
+                                result = a / b;
+                                Console.WriteLine($"Результат: {result}");
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Неправильно ввели операцию");
+                            break;
+                    }
+                } else { Console.WriteLine("Ошибка при вводе операций"); }
             }
-            else
-            {
-                Console.WriteLine("Ошибка при вводе b");
-            }
+            else { Console.WriteLine("Ошибка при вводе b"); }
         }
-        else
-        {
-            Console.WriteLine("Ошибка при вводе a");
-        }
+        else { Console.WriteLine("Ошибка при вводе a"); }
     }
 }
